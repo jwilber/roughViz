4b9d6bbac88c1b07d20c7a19040ea4eb43b7ac67
Jared Wilber <j.wilber92@gmail.com>
2019-09-18 12:05:37 -0700

    transpile stuff

---
+++ babel.config.js
@@ -0,0 +1,13 @@
+// babel.config.js
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
+        },
+      },
+    ],
+  ],
+};
