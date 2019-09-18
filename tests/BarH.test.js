0fae7f48c092650a8c5836ce35375ea2c349192d
Jared Wilber <j.wilber92@gmail.com>
2019-09-18 12:01:18 -0700

    init tests

---
+++ tests/BarH.test.js
@@ -0,0 +1,20 @@
+import roughBars from '../src';
+
+describe("Filter function", () => {
+  test("it should filter by a search term (link)", () => {
+    const barChart = new roughBars.BarH(
+	  {
+	   
+	  }
+	);
+
+	// let padding = barChart.padding;
+	let padding = 0.15
+
+
+
+
+    // const output = [{ id: 3, url: "https://www.link3.dev" }];
+    expect(padding).toEqual(0.15);
+  });
+});
---
+++ tests/Donut.test.js
@@ -0,0 +1,16 @@
+import roughBars from '../src';
+
+describe("Test Chart", () => {
+  test("Attributes should correctly propagate", () => {
+
+
+	// let padding = barChart.padding;
+	let padding = 0.15
+
+
+
+
+    // const output = [{ id: 3, url: "https://www.link3.dev" }];
+    expect(padding).toEqual(0.15);
+  });
+});
