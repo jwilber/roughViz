ce1145e4c81740ffee1a5916d75d24e4637fdec3
Jared Wilber <j.wilber92@gmail.com>
2019-09-23 12:22:27 -0700

    added Line

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import Bar from './Bar';
 import BarH from './BarH';
 import Donut from './Donut';
+import Line from './Line';
 import Pie from './Pie';
 import Scatter from './Scatter';
 
 
 module.exports = {
-  Bar, BarH, Donut, Pie, Scatter,
+  Bar, BarH, Donut, Line, Pie, Scatter,
 };
