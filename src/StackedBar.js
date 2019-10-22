44af23c6db0b831ce3cd1bfea242149aa9ffac95
jolo-dev <johnyscrazy@googlemail.com>
2019-10-22 08:44:47 +0200

    Change coloring of bars

--- src/StackedBar.js
+++ src/StackedBar.js
@@ -1,10 +1,11 @@
-import { addFontGaegu, addFontIndieFlower } from './utils/addFonts';
-import { max } from 'd3-array';
-import { axisBottom, axisLeft } from 'd3-axis';
-import { csv, tsv } from 'd3-fetch';
-import { scaleBand, scaleLinear, scaleOrdinal } from 'd3-scale';
-import { mouse, select, selectAll } from 'd3-selection';
-import rough from 'roughjs/dist/rough.umd';
+import { addFontGaegu, addFontIndieFlower } from "./utils/addFonts";
+import { max } from "d3-array";
+import { axisBottom, axisLeft } from "d3-axis";
+import { csv, tsv } from "d3-fetch";
+import { scaleBand, scaleLinear, scaleOrdinal } from "d3-scale";
+import { mouse, select, selectAll } from "d3-selection";
+import { colors } from "./utils/colors";
+import rough from "roughjs/dist/rough.umd";
 
 const roughCeiling = roughness => {
   let roughVal = roughness > 20 ? 20 : roughness;
@@ -19,10 +20,10 @@ class StackedBar {
     this.element = opts.element;
     this.margin = opts.margin || { top: 50, right: 20, bottom: 70, left: 100 };
     this.title = opts.title;
-    this.color = opts.color || 'skyblue';
-    this.highlight = opts.highlight || 'coral';
+    this.colors = opts.colors || colors;
+    this.highlight = opts.highlight || "coral";
     this.roughness = roughCeiling(opts.roughness) || 1;
-    this.stroke = opts.stroke || 'black';
+    this.stroke = opts.stroke || "black";
     this.strokeWidth = opts.strokeWidth || 1;
     this.axisStrokeWidth = opts.axisStrokeWidth || 0.5;
     this.axisRoughness = opts.axisRoughness || 0.5;
@@ -34,15 +35,15 @@ class StackedBar {
     this.interactive = opts.interactive !== false;
     this.titleFontSize = opts.titleFontSize;
     this.axisFontSize = opts.axisFontSize;
-    this.tooltipFontSize = opts.tooltipFontSize || '0.95rem';
+    this.tooltipFontSize = opts.tooltipFontSize || "0.95rem";
     this.font = opts.font || 0;
-    this.dataFormat = typeof opts.data === 'object' ? 'object' : 'file';
-    this.labels = this.dataFormat === 'object' ? 'labels' : opts.labels;
-    this.values = this.dataFormat === 'object' ? 'values' : opts.values;
+    this.dataFormat = typeof opts.data === "object" ? "object" : "file";
+    this.labels = this.dataFormat === "object" ? "labels" : opts.labels;
+    this.values = this.dataFormat === "object" ? "values" : opts.values;
     this.padding = opts.padding || 0.1;
-    this.xLabel = opts.xLabel || '';
-    this.yLabel = opts.yLabel || '';
-    this.labelFontSize = opts.labelFontSize || '1rem';
+    this.xLabel = opts.xLabel || "";
+    this.yLabel = opts.yLabel || "";
+    this.labelFontSize = opts.labelFontSize || "1rem";
     // new width
     this.initChartValues(opts);
     // resolve font
@@ -50,7 +51,7 @@ class StackedBar {
     // create the chart
     this.drawChart = this.resolveData(opts.data);
     this.drawChart();
-    if (opts.title !== 'undefined') this.setTitle(opts.title);
+    if (opts.title !== "undefined") this.setTitle(opts.title);
   }
 
   initChartValues(opts) {
@@ -58,22 +59,22 @@ class StackedBar {
     let height = opts.height ? opts.height : 450;
     this.width = width - this.margin.left - this.margin.right;
     this.height = height - this.margin.top - this.margin.bottom;
-    this.roughId = this.el + '_svg';
+    this.roughId = this.el + "_svg";
     this.graphClass = this.el.substring(1, this.el.length);
-    this.interactionG = 'g.' + this.graphClass;
+    this.interactionG = "g." + this.graphClass;
     this.setSvg();
   }
 
   setSvg() {
     this.svg = select(this.el)
-      .append('svg')
-      .attr('width', this.width + this.margin.left + this.margin.right)
-      .attr('height', this.height + this.margin.top + this.margin.bottom)
-      .append('g')
-      .attr('id', this.roughId)
+      .append("svg")
+      .attr("width", this.width + this.margin.left + this.margin.right)
+      .attr("height", this.height + this.margin.top + this.margin.bottom)
+      .append("g")
+      .attr("id", this.roughId)
       .attr(
-        'transform',
-        'translate(' + this.margin.left + ',' + this.margin.top + ')'
+        "transform",
+        "translate(" + this.margin.left + "," + this.margin.top + ")"
       );
   }
 
@@ -81,16 +82,16 @@ class StackedBar {
     if (
       this.font === 0 ||
       this.font === undefined ||
-      this.font.toString().toLowerCase() === 'gaegu'
+      this.font.toString().toLowerCase() === "gaegu"
     ) {
       addFontGaegu(this.svg);
-      this.fontFamily = 'gaeguregular';
+      this.fontFamily = "gaeguregular";
     } else if (
       this.font === 1 ||
-      this.font.toString().toLowerCase() === 'indie flower'
+      this.font.toString().toLowerCase() === "indie flower"
     ) {
       addFontIndieFlower(this.svg);
-      this.fontFamily = 'indie_flowerregular';
+      this.fontFamily = "indie_flowerregular";
     } else {
       this.fontFamily = this.font;
     }
@@ -98,8 +99,8 @@ class StackedBar {
 
   // add this to abstract base
   resolveData(data) {
-    if (typeof data === 'string') {
-      if (data.includes('.csv')) {
+    if (typeof data === "string") {
+      if (data.includes(".csv")) {
         return () => {
           csv(data).then(d => {
             let t = 0;
@@ -113,7 +114,7 @@ class StackedBar {
             this.drawFromFile();
           });
         };
-      } else if (data.includes('.tsv')) {
+      } else if (data.includes(".tsv")) {
         return () => {
           tsv(data).then(d => {
             let t = 0;
@@ -141,7 +142,7 @@ class StackedBar {
       .rangeRound([0, this.width])
       .padding(this.padding)
       .domain(
-        this.dataFormat === 'file'
+        this.dataFormat === "file"
           ? this.data.map(d => d[this.labels])
           : this.data[this.labels]
       );
@@ -155,7 +156,7 @@ class StackedBar {
         0,
         max(this.data, d => {
           return d.total;
-        }),
+        })
       ])
       .nice();
 
@@ -163,43 +164,43 @@ class StackedBar {
     var keys = this.data.columns;
     this.zScale = scaleOrdinal()
       .range([
-        '#98abc5',
-        '#8a89a6',
-        '#7b6888',
-        '#6b486b',
-        '#a05d56',
-        '#d0743c',
-        '#ff8c00',
+        "#98abc5",
+        "#8a89a6",
+        "#7b6888",
+        "#6b486b",
+        "#a05d56",
+        "#d0743c",
+        "#ff8c00"
       ])
       .domain(keys);
   }
 
   addLabels() {
     // xLabel
-    if (this.xLabel !== '') {
+    if (this.xLabel !== "") {
       this.svg
-        .append('text')
-        .attr('x', this.width / 2)
-        .attr('y', this.height + this.margin.bottom / 2)
-        .attr('dx', '1em')
-        .attr('class', 'labelText')
-        .style('text-anchor', 'middle')
-        .style('font-family', this.fontFamily)
-        .style('font-size', this.labelFontSize)
+        .append("text")
+        .attr("x", this.width / 2)
+        .attr("y", this.height + this.margin.bottom / 2)
+        .attr("dx", "1em")
+        .attr("class", "labelText")
+        .style("text-anchor", "middle")
+        .style("font-family", this.fontFamily)
+        .style("font-size", this.labelFontSize)
         .text(this.xLabel);
     }
     // yLabel
-    if (this.yLabel !== '') {
+    if (this.yLabel !== "") {
       this.svg
-        .append('text')
-        .attr('transform', 'rotate(-90)')
-        .attr('y', 0 - this.margin.left / 1.4)
-        .attr('x', 0 - this.height / 2)
-        .attr('dy', '1em')
-        .attr('class', 'labelText')
-        .style('text-anchor', 'middle')
-        .style('font-family', this.fontFamily)
-        .style('font-size', this.labelFontSize)
+        .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - this.margin.left / 1.4)
+        .attr("x", 0 - this.height / 2)
+        .attr("dy", "1em")
+        .attr("class", "labelText")
+        .style("text-anchor", "middle")
+        .style("font-family", this.fontFamily)
+        .style("font-size", this.labelFontSize)
         .text(this.yLabel);
     }
   }
@@ -209,40 +210,40 @@ class StackedBar {
 
     // x-axis
     this.svg
-      .append('g')
-      .attr('transform', 'translate(0,' + this.height + ')')
+      .append("g")
+      .attr("transform", "translate(0," + this.height + ")")
       .call(xAxis)
-      .attr('class', `xAxis${this.graphClass}`)
-      .selectAll('text')
-      .attr('transform', 'translate(-10,0)rotate(-45)')
-      .style('text-anchor', 'end')
-      .style('font-family', this.fontFamily)
+      .attr("class", `xAxis${this.graphClass}`)
+      .selectAll("text")
+      .attr("transform", "translate(-10,0)rotate(-45)")
+      .style("text-anchor", "end")
+      .style("font-family", this.fontFamily)
       .style(
-        'font-size',
+        "font-size",
         this.axisFontSize === undefined
           ? `${Math.min(0.8, Math.min(this.width, this.height) / 140)}rem`
           : this.axisFontSize
       )
-      .style('opacity', 0.9);
+      .style("opacity", 0.9);
 
     // y-axis
     const yAxis = axisLeft(this.yScale).tickSize(0);
     this.svg
-      .append('g')
+      .append("g")
       .call(yAxis)
-      .attr('class', `yAxis${this.graphClass}`)
-      .selectAll('text')
-      .style('font-family', this.fontFamily)
+      .attr("class", `yAxis${this.graphClass}`)
+      .selectAll("text")
+      .style("font-family", this.fontFamily)
       .style(
-        'font-size',
+        "font-size",
         this.axisFontSize === undefined
           ? `${Math.min(0.95, Math.min(this.width, this.height) / 140)}rem`
           : this.axisFontSize
       )
-      .style('opacity', 0.9);
+      .style("opacity", 0.9);
 
     // hide original axes
-    selectAll('path.domain').attr('stroke', 'transparent');
+    selectAll("path.domain").attr("stroke", "transparent");
   }
 
   makeAxesRough(roughSvg, rcAxis) {
@@ -252,138 +253,139 @@ class StackedBar {
     let roughYAxisClass = `rough-${yAxisClass}`;
 
     select(`.${xAxisClass}`)
-      .selectAll('path.domain')
+      .selectAll("path.domain")
       .each(function(d, i) {
         let pathD = select(this)
           .node()
-          .getAttribute('d');
+          .getAttribute("d");
         let roughXAxis = rcAxis.path(pathD, {
-          fillStyle: 'hachure',
+          fillStyle: "hachure"
         });
-        roughXAxis.setAttribute('class', roughXAxisClass);
+        roughXAxis.setAttribute("class", roughXAxisClass);
         roughSvg.appendChild(roughXAxis);
       });
     selectAll(`.${roughXAxisClass}`).attr(
-      'transform',
+      "transform",
       `translate(0, ${this.height})`
     );
 
     select(`.${yAxisClass}`)
-      .selectAll('path.domain')
+      .selectAll("path.domain")
       .each(function(d, i) {
         let pathD = select(this)
           .node()
-          .getAttribute('d');
+          .getAttribute("d");
         let roughYAxis = rcAxis.path(pathD, {
-          fillStyle: 'hachure',
+          fillStyle: "hachure"
         });
-        roughYAxis.setAttribute('class', roughYAxisClass);
+        roughYAxis.setAttribute("class", roughYAxisClass);
         roughSvg.appendChild(roughYAxis);
       });
   }
 
   setTitle(title) {
     this.svg
-      .append('text')
-      .attr('x', this.width / 2)
-      .attr('y', 0 - this.margin.top / 2)
-      .attr('class', 'title')
-      .attr('text-anchor', 'middle')
+      .append("text")
+      .attr("x", this.width / 2)
+      .attr("y", 0 - this.margin.top / 2)
+      .attr("class", "title")
+      .attr("text-anchor", "middle")
       .style(
-        'font-size',
+        "font-size",
         this.titleFontSize === undefined
           ? `${Math.min(40, Math.min(this.width, this.height) / 5)}px`
           : this.titleFontSize
       )
-      .style('font-family', this.fontFamily)
-      .style('opacity', 0.8)
+      .style("font-family", this.fontFamily)
+      .style("opacity", 0.8)
       .text(title);
   }
 
   addInteraction(y) {
     selectAll(this.interactionG)
-      .data(this.dataFormat === 'file' ? this.data : this.data.values)
-      .append('rect')
-      .attr('x', (d, i) => {
-        return this.dataFormat === 'file'
+      .data(this.dataFormat === "file" ? this.data : this.data.values)
+      .append("rect")
+      .attr("x", (d, i) => {
+        return this.dataFormat === "file"
           ? this.xScale(d[this.labels])
           : this.xScale(this.data[this.labels][i]);
       })
-      .attr('y', (d, i) => {
-        return this.dataFormat === 'file'
+      .attr("y", (d, i) => {
+        return this.dataFormat === "file"
           ? this.yScale(+d[y])
           : this.yScale(this.data[y][i]);
       })
-      .attr('width', this.xScale.bandwidth())
-      .attr('height', (d, i) => {
-        return this.dataFormat === 'file'
+      .attr("width", this.xScale.bandwidth())
+      .attr("height", (d, i) => {
+        return this.dataFormat === "file"
           ? this.height - this.yScale(+d[y])
           : this.height - this.yScale(this.data[y][i]);
       })
-      .attr('fill', 'transparent');
+      .attr("fill", "transparent");
 
     // create tooltip
     const Tooltip = select(this.el)
-      .append('div')
-      .style('opacity', 0)
-      .attr('class', 'tooltip')
-      .style('position', 'absolute')
-      .style('background-color', 'white')
-      .style('border', 'solid')
-      .style('border-width', '1px')
-      .style('border-radius', '5px')
-      .style('padding', '3px')
-      .style('font-family', this.fontFamily)
-      .style('font-size', this.tooltipFontSize)
-      .style('pointer-events', 'none');
+      .append("div")
+      .style("opacity", 0)
+      .attr("class", "tooltip")
+      .style("position", "absolute")
+      .style("background-color", "white")
+      .style("border", "solid")
+      .style("border-width", "1px")
+      .style("border-radius", "5px")
+      .style("padding", "3px")
+      .style("font-family", this.fontFamily)
+      .style("font-size", this.tooltipFontSize)
+      .style("pointer-events", "none");
 
     // event functions
     var mouseover = function(d) {
-      Tooltip.style('opacity', 1);
+      Tooltip.style("opacity", 1);
     };
     let that = this;
 
     var mousemove = function(d) {
-      let attrX = select(this).attr('attrX');
-      let attrY = select(this).attr('attrY');
+      let attrX = select(this).attr("attrX");
+      let attrY = select(this).attr("attrY");
+      let keyY = select(this).attr("keyY");
       let mousePos = mouse(this);
       // get size of enclosing div
-      Tooltip.html(`<b>${attrX}</b>: ${attrY}`)
-        .style('opacity', 0.95)
-        .attr('class', function(d) {})
+      Tooltip.html(`<h4>${attrX}</h4> <b>${keyY}</b>: ${attrY}`)
+        .style("opacity", 0.95)
+        .attr("class", function(d) {})
         .style(
-          'transform',
+          "transform",
           `translate(${mousePos[0] + that.margin.left}px, 
           ${mousePos[1] -
             (that.height + that.margin.top + that.margin.bottom)}px)`
         );
     };
     var mouseleave = function(d) {
-      Tooltip.style('opacity', 0);
+      Tooltip.style("opacity", 0);
     };
 
     // d3 event handlers
-    selectAll(this.interactionG).on('mouseover', function() {
+    selectAll(this.interactionG).on("mouseover", function() {
       mouseover();
       select(this)
-        .select('path')
-        .style('stroke', that.highlight);
+        .select("path")
+        .style("stroke", that.highlight);
       select(this)
-        .selectAll('path:nth-child(2)')
-        .style('stroke-width', that.strokeWidth + 1.2);
+        .selectAll("path:nth-child(2)")
+        .style("stroke-width", that.strokeWidth + 1.2);
     });
 
-    selectAll(this.interactionG).on('mouseout', function() {
+    selectAll(this.interactionG).on("mouseout", function() {
       mouseleave();
       select(this)
-        .select('path')
-        .style('stroke', that.color);
+        .select("path")
+        .style("stroke", that.color);
       select(this)
-        .selectAll('path:nth-child(2)')
-        .style('stroke-width', that.strokeWidth);
+        .selectAll("path:nth-child(2)")
+        .style("stroke-width", that.strokeWidth);
     });
 
-    selectAll(this.interactionG).on('mousemove', mousemove);
+    selectAll(this.interactionG).on("mousemove", mousemove);
   }
 
   initRoughObjects() {
@@ -391,18 +393,18 @@ class StackedBar {
     this.rcAxis = rough.svg(this.roughSvg, {
       options: {
         strokeWidth: this.axisStrokeWidth,
-        roughness: this.axisRoughness,
-      },
+        roughness: this.axisRoughness
+      }
     });
     this.rc = rough.svg(this.roughSvg, {
       options: {
-        fill: this.color,
-        stroke: this.stroke === 'none' ? undefined : this.stroke,
+        // fill: this.color,
+        stroke: this.stroke === "none" ? undefined : this.stroke,
         strokeWidth: this.innerStrokeWidth,
         roughness: this.roughness,
         bowing: this.bowing,
-        fillStyle: this.fillStyle,
-      },
+        fillStyle: this.fillStyle
+      }
     });
   }
 
@@ -422,18 +424,18 @@ class StackedBar {
         this.height - this.yScale(+d),
         {
           simplification: this.simplification,
-          fillWeight: this.fillWeight,
+          fillWeight: this.fillWeight
         }
       );
       let roughNode = this.roughSvg.appendChild(node);
-      roughNode.setAttribute('class', this.graphClass);
-      roughNode.setAttribute('attrX', this.data[this.labels][i]);
-      roughNode.setAttribute('attrY', +d);
+      roughNode.setAttribute("class", this.graphClass);
+      roughNode.setAttribute("attrX", this.data[this.labels][i]);
+      roughNode.setAttribute("attrY", +d);
     });
 
     selectAll(this.interactionG)
-      .selectAll('path:nth-child(2)')
-      .style('stroke-width', this.strokeWidth);
+      .selectAll("path:nth-child(2)")
+      .style("stroke-width", this.strokeWidth);
     // If desired, add interactivity
     if (this.interactive === true) {
       this.addInteraction();
@@ -449,29 +451,33 @@ class StackedBar {
     // Add Stackedbarplot
     this.data.forEach(d => {
       var keys = Object.keys(d);
-      keys.forEach(y => {
-        if (y !== this.labels) {
+      keys.forEach((y, i) => {
+        if (y !== this.labels || y !== "total") {
+          console.log(y);
           let node = this.rc.rectangle(
             this.xScale(d[this.labels]),
             this.yScale(+d[y]),
             this.xScale.bandwidth(),
             this.height - this.yScale(+d[y]),
             {
+              fill: this.colors[i],
+              stroke: this.colors[i],
               simplification: this.simplification,
-              fillWeight: this.fillWeight,
+              fillWeight: this.fillWeight
             }
           );
           let roughNode = this.roughSvg.appendChild(node);
-          roughNode.setAttribute('class', this.graphClass);
-          roughNode.setAttribute('attrX', d[this.labels]);
-          roughNode.setAttribute('attrY', +d[y]);
+          roughNode.setAttribute("class", this.graphClass);
+          roughNode.setAttribute("attrX", d[this.labels]);
+          roughNode.setAttribute("keyY", y);
+          roughNode.setAttribute("attrY", +d[y]);
         }
       });
     });
 
     selectAll(this.interactionG)
-      .selectAll('path:nth-child(2)')
-      .style('stroke-width', this.strokeWidth);
+      .selectAll("path:nth-child(2)")
+      .style("stroke-width", this.strokeWidth);
     // If desired, add interactivity
     if (this.interactive === true) {
       this.addInteraction();
