a6128403082c15c72f8df8a183e9ef7fb5493329
Wilber <wilberjw@amazon.com>
2021-05-02 01:51:09 -0700

    make example chart height smaller

--- examples/index.js
+++ examples/index.js
@@ -1,6 +1,6 @@
 // /* eslint max-len: ['error', { 'code': 150 }]*/
 // /* eslint-disable no-new */
-import roughViz from '../src';
+import roughViz from "../src";
 
 // new roughViz.Line(
 //   {
@@ -130,30 +130,30 @@ import roughViz from '../src';
 // );
 
 new roughViz.BarH({
-  element: '#vis3',
+  element: "#vis3",
   data: {
     labels: [
-      'Reggae',
-      'Rap',
-      'R&B',
-      'Dance House',
-      'Hip Hop',
-      'Trap',
-      'tre',
-      'eifj',
+      "Reggae",
+      "Rap",
+      "R&B",
+      "Dance House",
+      "Hip Hop",
+      "Trap",
+      "tre",
+      "eifj",
     ],
     values: [240, 40, 400, 90, 75, 315, 43, 1500],
   },
   width: window.innerWidth / 2,
-  title: 'Genres',
+  title: "Genres",
   roughness: 1,
-  highlight: 'chocolate',
-  stroke: 'grey',
+  highlight: "chocolate",
+  stroke: "grey",
   padding: 0.15,
   bowing: 6,
   innerStrokeWidth: 0.5,
-  color: 'white',
-  fillStyle: 'dashed',
+  color: "white",
+  fillStyle: "dashed",
 });
 
 // new roughViz.Bar(
@@ -213,48 +213,48 @@ new roughViz.BarH({
 //   }
 // );
 
-new roughViz.Scatter(
-  {
-    element: '#vis0',
-    data: 'https://raw.githubusercontent.com/uiuc-cse/data-fa14/gh-pages/data/iris.csv',
-    title: 'Iris',
-    x: 'sepal_width',
-    y: 'petal_length',
-    colorVar: 'species',
-    highlightLabel: 'species',
-    fillWeight: 5.1,
-    radius: 12,
-    colors: ['pink', 'coral', 'skyblue'],
-    stroke: 'black',
-    strokeWidth: .4,
-    roughness: 1,
-    // strokeWidth: 0.5,
-    // titleFontSize: '2rem',
-    // axesFontSize: '1.5rem',
-    // axisStrokeWidth: 0.4,
-    // axisRoughness: 2,
-    width: 400,
-    height: 500,
-    font: 0,
-    curbZero: false,
-    fillStyle: 'crss-hatch',
-    // highlight: 'blue'
-})
+new roughViz.Scatter({
+  element: "#vis0",
+  data:
+    "https://raw.githubusercontent.com/uiuc-cse/data-fa14/gh-pages/data/iris.csv",
+  title: "Iris",
+  x: "sepal_width",
+  y: "petal_length",
+  colorVar: "species",
+  highlightLabel: "species",
+  fillWeight: 5.1,
+  radius: 12,
+  colors: ["pink", "coral", "skyblue"],
+  stroke: "black",
+  strokeWidth: 0.4,
+  roughness: 1,
+  // strokeWidth: 0.5,
+  // titleFontSize: '2rem',
+  // axesFontSize: '1.5rem',
+  // axisStrokeWidth: 0.4,
+  // axisRoughness: 2,
+  width: 400,
+  height: 500,
+  font: 0,
+  curbZero: false,
+  fillStyle: "crss-hatch",
+  // highlight: 'blue'
+});
 
 new roughViz.Donut({
-  element: '#vis1',
+  element: "#vis1",
   data:
-    'https://raw.githubusercontent.com/jwilber/jenkem_data/master/regions.json',
-  title: 'Regions',
-  labels: 'region',
-  values: 'count',
+    "https://raw.githubusercontent.com/jwilber/jenkem_data/master/regions.json",
+  title: "Regions",
+  labels: "region",
+  values: "count",
   roughness: 0,
-  colors: ['pink', 'coral', 'teal', 'skyblue'],
-  highlight: 'gold',
+  colors: ["pink", "coral", "teal", "skyblue"],
+  highlight: "gold",
   strokeWidth: 2,
-  titleFontSize: '25px',
+  titleFontSize: "25px",
   fillWeight: 1.5,
-  fillStyle: 'zigzag-line',
+  fillStyle: "zigzag-line",
   width: window.innerWidth / 2,
   height: 450,
 });
@@ -284,18 +284,18 @@ new roughViz.Donut({
 // );
 
 new roughViz.Pie({
-  element: '#vis2',
+  element: "#vis2",
   data: {
-    labels: ['Mike', 'Jah',],
-    values: [13, 10, ],
+    labels: ["Mike", "Jah"],
+    values: [13, 10],
   },
   title: '"Yarn" Plot (Pie)',
   width: window.innerWidth / 4,
   roughness: 2,
-  colors: ['red', 'orange', 'blue', 'skyblue', 'red', 'green', 'black', 'grey'],
-  stroke: 'black',
+  colors: ["red", "orange", "blue", "skyblue", "red", "green", "black", "grey"],
+  stroke: "black",
   strokeWidth: 3,
-  fillStyle: 'cross-hatch',
+  fillStyle: "cross-hatch",
   fillWeight: 3.5,
 });
 
@@ -473,20 +473,20 @@ new roughViz.Pie({
 // );
 
 new roughViz.Bar({
-  element: '#example',
+  element: "#example",
   data:
-    'https://gist.githubusercontent.com/mbostock/3310560/raw/98311dc46685ed02588afdcb69e5fa296febc1eb/letter-frequency.tsv',
-  labels: 'letter',
-  values: 'frequency',
-  height: window.innerHeight * 0.7,
+    "https://gist.githubusercontent.com/mbostock/3310560/raw/98311dc46685ed02588afdcb69e5fa296febc1eb/letter-frequency.tsv",
+  labels: "letter",
+  values: "frequency",
+  height: window.innerHeight * 0.6,
   width: window.innerWidth * 0.8,
   roughness: 3,
-  color: 'pink',
+  color: "pink",
   fillWeight: 1,
   strokeWidth: 0.5,
-  fillStyle: 'cross-hatch',
+  fillStyle: "cross-hatch",
   // roughness: 0,
-  stroke: 'black',
+  stroke: "black",
 });
 
 // new roughViz.BarH(
