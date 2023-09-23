e1d61c2c02a07cf3871da3094cc5a80fa0a8fde5
Jared Wilber <jdwlbr@gmail.com>
2023-09-23 12:51:20 -0700

    make barchart responsive

--- src/Bar.js
+++ src/Bar.js
@@ -14,7 +14,7 @@ class Bar extends Chart {
 
     // load in arguments from config object
     this.data = opts.data;
-    this.margin = opts.margin || { top: 50, right: 20, bottom: 70, left: 100 };
+    this.margin = opts.margin || { top: 20, right: 10, bottom: 20, left: 20 };
     this.color = opts.color || "red";
     this.highlight = opts.highlight || "coral";
     this.roughness = roughCeiling({ roughness: opts.roughness });
@@ -33,6 +33,8 @@ class Bar extends Chart {
     this.xLabel = opts.xLabel || "";
     this.yLabel = opts.yLabel || "";
     this.labelFontSize = opts.labelFontSize || "1rem";
+    this.responsive = true;
+    this.boundRedraw = this.redraw.bind(this, opts);
     // new width
     this.initChartValues(opts);
     // resolve font
@@ -41,11 +43,36 @@ class Bar extends Chart {
     this.drawChart = this.resolveData(opts.data);
     this.drawChart();
     if (opts.title !== "undefined") this.setTitle(opts.title);
+    window.addEventListener("resize", this.resizeHandler.bind(this));
+  }
+
+  resizeHandler() {
+    if (this.responsive) {
+      this.boundRedraw();
+    }
+  }
+
+  redraw(opts) {
+    // 1. Remove the current SVG associated with the chart.
+    select(this.el).select("svg").remove();
+
+    // 2. Recalculate the size of the container.
+    this.initChartValues(opts);
+
+    // 3. Redraw everything.
+    this.resolveFont();
+    this.drawChart = this.resolveData(opts.data);
+    this.drawChart();
+
+    if (opts.title !== "undefined") {
+      this.setTitle(opts.title);
+    }
   }
 
   initChartValues(opts) {
-    const width = opts.width ? opts.width : 350;
-    const height = opts.height ? opts.height : 450;
+    const divDimensions = select(this.el).node().getBoundingClientRect();
+    const width = divDimensions.width;
+    const height = divDimensions.height;
     this.width = width - this.margin.left - this.margin.right;
     this.height = height - this.margin.top - this.margin.bottom;
     this.roughId = this.el + "_svg";
@@ -271,7 +298,7 @@ class Bar extends Chart {
       .style("pointer-events", "none");
 
     // event functions
-    var mouseover = function (d) {
+    let mouseover = function (d) {
       Tooltip.style("opacity", 1);
     };
     const that = this;
@@ -292,7 +319,8 @@ class Bar extends Chart {
           }px)`
         );
     };
-    var mouseleave = function (d) {
+
+    let mouseleave = function (d) {
       Tooltip.style("opacity", 0);
     };
 
