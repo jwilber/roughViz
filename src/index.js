e57b6d0403e80668c808132825810c21442d7299
Jared Wilber <jdwlbr@gmail.com>
2023-09-30 20:12:37 -0700

    add force and network charts

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
-import Bar from './Bar';
-import BarH from './BarH';
-import Donut from './Donut';
-import Line from './Line';
-import Pie from './Pie';
-import Scatter from './Scatter';
-import StackedBar from './StackedBar';
+import Bar from "./Bar";
+import BarH from "./BarH";
+import Donut from "./Donut";
+import Line from "./Line";
+import Network from "./Network";
+import Force from "./Force";
+import Pie from "./Pie";
+import Scatter from "./Scatter";
+import StackedBar from "./StackedBar";
 
 module.exports = {
   Bar,
   BarH,
   Donut,
   Line,
+  Network,
+  Force,
   Pie,
   Scatter,
   StackedBar,
