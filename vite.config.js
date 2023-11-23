32667f28b3a0ef47efad10232409cf177715f2d2
Jared Wilber <jdwlbr@gmail.com>
2023-11-22 16:20:37 -0800

    add commonjs

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
     lib: {
       entry: "src/index.js",
       name: "roughViz",
-      formats: ["es", "umd"],
+      formats: ["es", "umd", "cjs"],
       fileName: (format) => `roughviz.${format}.js`,
     },
     rollupOptions: {
